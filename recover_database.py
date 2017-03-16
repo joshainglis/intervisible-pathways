776f39499e4b400573882d6040136ef6400a06a1
Josha Inglis <joshainglis@gmail.com>
2017-03-16 19:27:38 +1000

    Remove vestigial code

--- recover_database.py
+++
@@ -1,8 +0,0 @@
-import arcpy
-from os.path import join, expanduser
-
-geodatabase = join(expanduser('~'), 'Documents', 'ArcGIS', 'recoveredDefault.gdb')
-output_location = join(expanduser('~'), 'Documents', 'ArcGIS')
-recovered_name = "recoveredDefault2.gdb"
-
-arcpy.RecoverFileGDB_management(geodatabase, output_location, recovered_name)
